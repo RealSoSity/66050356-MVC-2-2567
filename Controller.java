ff2539e59b4b8c8acfb376edea7458d0b81296a0
Real <realsosity@gmail.com>
2025-02-09 10:35:24 +0700

    Nine commit

--- Controller.java
+++ Controller.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
-    
+    //Attribute and Set-up
+    private View view;
+    private CsvRead csvRead;
+    private CsvWritter csvWritter;
+    private List<PetsModel> pets = new ArrayList<>();
+
+    public Controller(View view, CsvRead csvRead, CsvWritter csvWritter){
+        this.view = view;
+        this.csvRead = csvRead;
+        this.csvWritter = csvWritter;
+        pets = csvRead.getPetsList();
+    }
+
+    private void addPets(PetsModel pet){
+        pets.add(pet);
+        csvWritter.addPets(pets);
+    }
+
+    public int getAcceptCount(String type){
+        return (int) pets.stream().filter(p -> p.getType().equals(type) && p.getAccept()).count();
+    }
+
+    public int getRejectCount(){
+        return (int) pets.stream().filter(p -> !p.getAccept()).count();
+    }
+
+
 }
--- CsvWritter.java
+++ CsvWritter.java
@@ -4,8 +4,14 @@ import java.io.IOException;
 import java.util.List;
 
 public class CsvWritter {
+
+    private String file_path;
     //Write data into CSV
-    public CsvWritter(String file_path, List<PetsModel> pets){
+    public CsvWritter(String file_path){
+        this.file_path = file_path;
+    }
+
+    public void addPets(List<PetsModel> pets){
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file_path))) {
             for (PetsModel pet : pets) {
                 writer.write(pet.toCSV());
